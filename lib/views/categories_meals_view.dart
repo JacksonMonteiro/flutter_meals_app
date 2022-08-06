3f86d940238e719eba04da65589c59095a4a66f3
jacksonmonteiro <infor.jackson324@gmail.com>
2022-08-06 16:05:15 -0300

    show meals by category in categories meals views file

--- lib/views/categories_meals_view.dart
+++ lib/views/categories_meals_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals/data/dummy_data.dart';
 import 'package:meals/models/category.dart';
 
 class CategoriesMealsView extends StatelessWidget {
@@ -9,6 +10,9 @@ class CategoriesMealsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final category = ModalRoute.of(context)?.settings.arguments as Category;
+    final categoryMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
     // final MediaQueryData mQuery = MediaQuery.of(context);
 
     AppBar appBar = AppBar(
@@ -26,8 +30,9 @@ class CategoriesMealsView extends StatelessWidget {
 
     return Scaffold(
       appBar: appBar,
-      body: const Center(
-        child: Text('Receitas por categoria'),
+      body: ListView.builder(
+        itemCount: categoryMeals.length,
+        itemBuilder: (context, index) => Text(categoryMeals[index].title),
       ),
     );
   }
