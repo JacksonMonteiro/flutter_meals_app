f92771d506b665f757b846a0a529558a765832bf
jacksonmonteiro <infor.jackson324@gmail.com>
2022-08-06 15:30:39 -0300

    first screen navigation

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -3,30 +3,44 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:meals/models/category.dart';
+import 'package:meals/views/categories_meals_view.dart';
 
 class CategoryItem extends StatelessWidget {
   final Category category;
 
   const CategoryItem({Key? key, required this.category}) : super(key: key);
 
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (_) => const CategoriesMealsView(),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            category.color.withOpacity(0.5),
-            category.color,
-          ],
-          begin: Alignment.topCenter,
-          end: Alignment.bottomRight,
+    return InkWell(
+      onTap: () => _selectCategory(context),
+      borderRadius: BorderRadius.circular(16),
+      splashColor: Theme.of(context).primaryColor,
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.5),
+              category.color,
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(16),
         ),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Text(
-        category.title,
-        style: Theme.of(context).textTheme.titleMedium,
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
       ),
     );
   }
---
+++ lib/views/categories_meals_view.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class CategoriesMealsView extends StatelessWidget {
+  const CategoriesMealsView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final MediaQueryData mQuery = MediaQuery.of(context);
+
+    AppBar appBar = AppBar(
+      title: const Text(
+        'Receitas',
+        style: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+      backgroundColor: Theme.of(context).colorScheme.primary,
+    );
+
+    final double availableHeight =
+        mQuery.size.height - appBar.preferredSize.height - mQuery.padding.top;
+
+    return Scaffold(
+      appBar: appBar,
+      body: const Center(
+        child: Text('Receitas por categoria'),
+      ),
+    );
+  }
+}
