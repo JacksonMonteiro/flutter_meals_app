e69358061dedd50f1fa699738aeedb0c6e8b25b5
jacksonmonteiro <infor.jackson324@gmail.com>
2022-08-06 15:44:21 -0300

    passing data with constructor

--- lib/views/categories_meals_view.dart
+++ lib/views/categories_meals_view.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:meals/models/category.dart';
 
 class CategoriesMealsView extends StatelessWidget {
-  const CategoriesMealsView({Key? key}) : super(key: key);
+  final Category category;
+
+  const CategoriesMealsView({Key? key, required this.category})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     // final MediaQueryData mQuery = MediaQuery.of(context);
 
     AppBar appBar = AppBar(
-      title: const Text(
-        'Receitas',
-        style: TextStyle(
+      title: Text(
+        category.title,
+        style: const TextStyle(
           color: Colors.white,
         ),
       ),
